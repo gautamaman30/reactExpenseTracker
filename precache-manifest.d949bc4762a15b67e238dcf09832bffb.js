5d8e0e5348c4de417e395bef65fe58ceea9a2fb5
gautamaman30 <gautamaman30@gmail.com>
2020-07-08 21:47:20 +0530

    Updates

---
+++ precache-manifest.d949bc4762a15b67e238dcf09832bffb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c633e047bfe4e98f5c9de2d03ccaf9",
+    "url": "/reactExpenseTracker/index.html"
+  },
+  {
+    "revision": "4f637a6c95f7a1aac694",
+    "url": "/reactExpenseTracker/static/css/main.f5daa4cb.chunk.css"
+  },
+  {
+    "revision": "f2cf3833cb58d4f57016",
+    "url": "/reactExpenseTracker/static/js/2.c57cdb69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactExpenseTracker/static/js/2.c57cdb69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f637a6c95f7a1aac694",
+    "url": "/reactExpenseTracker/static/js/main.0760db75.chunk.js"
+  },
+  {
+    "revision": "64367fb48ee8702eb9b6",
+    "url": "/reactExpenseTracker/static/js/runtime-main.589499ff.js"
+  }
+]);
